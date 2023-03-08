e15a0a15ab887789e475af9611889b7a32841857
SakshiRathi77 <78577141+SakshiRathi77@users.noreply.github.com>
2023-03-08 10:26:29 +0530

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+a=datetime.now()
+print(a)
+list1=list(map(float,str(a)))
+print(list1)
+# a=[5]
+# list1=tuple(map(str,a))
+# print(list1)
+# duration = list(map(int, map(float, (input_time - exists_inside).split(":"))))
